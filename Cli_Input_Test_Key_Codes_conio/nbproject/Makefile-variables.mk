cd7b9b1a548cfd034f07606bcb0eaaf9e842041e
mike <guliaevm@yahoo.com>
2020-06-05 09:41:06 +0400

    Test Key Codes: conio

---
+++ Cli_Input_Test_Key_Codes_conio/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU_DevCpp-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU_DevCpp-Linux
+CND_ARTIFACT_NAME_Debug=cli_input_test_key_codes_conio
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU_DevCpp-Linux/cli_input_test_key_codes_conio
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU_DevCpp-Linux/package
+CND_PACKAGE_NAME_Debug=cliinputtestkeycodesconio.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU_DevCpp-Linux/package/cliinputtestkeycodesconio.tar
+# Release configuration
+CND_PLATFORM_Release=GNU_DevCpp-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU_DevCpp-Linux
+CND_ARTIFACT_NAME_Release=cli_input_test_key_codes_conio
+CND_ARTIFACT_PATH_Release=dist/Release/GNU_DevCpp-Linux/cli_input_test_key_codes_conio
+CND_PACKAGE_DIR_Release=dist/Release/GNU_DevCpp-Linux/package
+CND_PACKAGE_NAME_Release=cliinputtestkeycodesconio.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU_DevCpp-Linux/package/cliinputtestkeycodesconio.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
