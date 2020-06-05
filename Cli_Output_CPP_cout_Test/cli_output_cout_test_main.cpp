d573cea530f74f00c6f6b73899da9510f5db9e5f
mike <guliaevm@yahoo.com>
2020-06-05 09:43:25 +0400

    Test Cli_Output_CPP_cout

---
+++ Cli_Output_CPP_cout_Test/cli_output_cout_test_main.cpp
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   cli_output_cout_test_main.cpp
+ * Author: mike
+ *
+ * Created on June 3, 2020, 12:18 PM
+ */
+
+#include "Cli_Output_cout.h"
+
+int main(int argc, char** argv) {
+    
+    Cli_Output_cout Cli_Output;
+    
+    Cli_Output.Output_Init();
+    
+    Cli_Output.Output_Char('X');
+    Cli_Output.Output_NewLine();
+    Cli_Output.Output_Str(" - Ok");
+    Cli_Output.Output_NewLine();
+    Cli_Output.Output_Str("End");
+    Cli_Output.Output_Return();
+    
+    char c;
+    cin.peek() >> c;
+    
+    Cli_Output.Output_Close();
+
+    return 0;
+}
