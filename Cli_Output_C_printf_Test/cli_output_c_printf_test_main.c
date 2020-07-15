acc51505575d4fecc4364855f224a00f31aff1a6
mike <guliaevm@yahoo.com>
2020-07-15 11:23:32 +0400

    Test Cli_Output_C_printf: C from C++

---
+++ Cli_Output_C_printf_Test/cli_output_c_printf_test_main.c
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   cli_output_c_printf_test_main.c
+ * Author: mike
+ *
+ * Created on July 2, 2020, 11:05 AM
+ */
+
+#include <stdio.h>
+
+#include "Cli_Output_C_printf.h"
+
+int main(int argc, char** argv) {
+
+    struct Cli_Output_C Cli_Output = Cli_Output_C_printf();
+
+    Cli_Output.Output_Init();
+
+    Cli_Output.Output_Char('X');
+    Cli_Output.Output_NewLine();
+    Cli_Output.Output_Str(" - Ok");
+    Cli_Output.Output_NewLine();
+    Cli_Output.Output_Str("End");
+    Cli_Output.Output_Return();
+
+    getchar();
+
+    Cli_Output.Output_Close();
+
+    return 0;
+}
