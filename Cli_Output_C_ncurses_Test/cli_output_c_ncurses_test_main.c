7619e6a413bf2ea5bdf1effabecefecc0e638303
mike <guliaevm@yahoo.com>
2020-07-15 11:26:41 +0400

    Test Cli_Output_C_ncurses: C from C++

---
+++ Cli_Output_C_ncurses_Test/cli_output_c_ncurses_test_main.c
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   cli_output_c_ncurses_test_main.c
+ * Author: mike
+ *
+ * Created on July 2, 2020, 11:58 AM
+ */
+
+#include "Cli_Output_C_ncurses.h"
+
+int main(int argc, char** argv) {
+
+    struct Cli_Output_C Cli_Output = Cli_Output_C_ncurses();
+
+    Cli_Output.Output_Init();
+
+    Cli_Output.Output_Char('X');
+    Cli_Output.Output_NewLine();
+    Cli_Output.Output_Str(" - Ok");
+    Cli_Output.Output_NewLine();
+    Cli_Output.Output_Str("End");
+    Cli_Output.Output_Return();
+
+    getch();
+
+    Cli_Output.Output_Close();
+
+    return 0;
+}
