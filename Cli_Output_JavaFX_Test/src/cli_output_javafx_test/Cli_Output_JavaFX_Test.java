ad72242b868cfa83e21716e228b3ada548638ea8
mike <guliaevm@yahoo.com>
2023-11-17 08:38:44 +0400

    Cli_Output_JavaFX_Test: stage.setTitle(...) - added

--- Cli_Output_JavaFX_Test/src/cli_output_javafx_test/Cli_Output_JavaFX_Test.java
+++ Cli_Output_JavaFX_Test/src/cli_output_javafx_test/Cli_Output_JavaFX_Test.java
@@ -24,6 +24,7 @@ public class Cli_Output_JavaFX_Test extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.setTitle("Cli Output JavaFX Test");
         stage.show();
     }
 
