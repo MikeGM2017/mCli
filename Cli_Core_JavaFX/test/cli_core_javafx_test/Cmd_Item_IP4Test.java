63f649b93839067009f5d4a02966facb39cab291
mike <guliaevm@yahoo.com>
2023-12-15 13:03:09 +0400

    Cli_Core_JavaFX: Cmd_Item_IP4/Test - added (not used)

---
+++ Cli_Core_JavaFX/test/cli_core_javafx_test/Cmd_Item_IP4Test.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cli_core_javafx_test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mike
+ */
+public class Cmd_Item_IP4Test {
+
+    String Test_Name = this.getClass().getSimpleName();
+
+    Cmd_Item_IP4 v = new Cmd_Item_IP4("", "");
+
+    class Cmd_Item_Test_Pair {
+
+        String Text;
+        Cmd_Item_Valid_Result Res;
+
+        Cmd_Item_Test_Pair(String text, Cmd_Item_Valid_Result res) {
+            Text = text;
+            Res = res;
+        }
+    }
+
+    final Cmd_Item_Test_Pair[] test_items = {
+        new Cmd_Item_Test_Pair("", Cmd_Item_Valid_Result.CMD_ITEM_EMPTY),
+        new Cmd_Item_Test_Pair("0.0.0.0", Cmd_Item_Valid_Result.CMD_ITEM_OK),
+        new Cmd_Item_Test_Pair("1.1.1.1", Cmd_Item_Valid_Result.CMD_ITEM_OK),
+        new Cmd_Item_Test_Pair("255.255.255.255", Cmd_Item_Valid_Result.CMD_ITEM_OK),
+        new Cmd_Item_Test_Pair("256.255.255.255", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("255.256.255.255", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("255.255.256.255", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("255.255.255.256", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("255.255.255", Cmd_Item_Valid_Result.CMD_ITEM_INCOMPLETE_STR),
+        new Cmd_Item_Test_Pair("255.255.255.", Cmd_Item_Valid_Result.CMD_ITEM_INCOMPLETE_STR),
+        new Cmd_Item_Test_Pair(".255.255.", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("255.255.255.25", Cmd_Item_Valid_Result.CMD_ITEM_OK),
+        new Cmd_Item_Test_Pair("255.255.255.2", Cmd_Item_Valid_Result.CMD_ITEM_OK),
+        new Cmd_Item_Test_Pair("255.255.255.0", Cmd_Item_Valid_Result.CMD_ITEM_OK),
+        new Cmd_Item_Test_Pair("255.255.255.00", Cmd_Item_Valid_Result.CMD_ITEM_OK),
+        new Cmd_Item_Test_Pair("255.255.255.000", Cmd_Item_Valid_Result.CMD_ITEM_OK),
+        new Cmd_Item_Test_Pair("255.255.255.0000", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("0255.255.255.255", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("255.0255.255.255", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("255.255.0255.255", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("255.255.255.0255", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("001.001.001.001", Cmd_Item_Valid_Result.CMD_ITEM_OK),
+        new Cmd_Item_Test_Pair("0001.001.001.001", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("001.0001.001.001", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("001.001.0001.001", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("001.001.001.0001", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("000.000.000.000", Cmd_Item_Valid_Result.CMD_ITEM_OK),
+        new Cmd_Item_Test_Pair("0000.000.000.000", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("000.0000.000.000", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("000.000.0000.000", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("000.000.000.0000", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("1.1..1", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("1..1.1.1", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("1.1..1.1", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("1.1.1..1", Cmd_Item_Valid_Result.CMD_ITEM_ERROR)
+    };
+
+    @Test
+    public void Test() {
+        System.out.println(Test_Name + ":");
+        Boolean test_res_total = true; // Ok
+        int test_res_total_count_failed = 0; // Ok
+        for (int i = 0; i < test_items.length; i++) {
+            Cmd_Item_Test_Pair test_item = test_items[i];
+            Cmd_Item_Valid_Result res_cmd_valid = v.Parse(test_item.Text);
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            sb.append(Integer.toString(i));
+            sb.append("] ");
+            sb.append("Test Str: \"");
+            sb.append(test_item.Text);
+            sb.append("\" res: \"");
+            sb.append(Cmd_Item_Valid_Result_Func.To_String(res_cmd_valid));
+            sb.append("\" ");
+            Boolean test_res = (res_cmd_valid == test_item.Res);
+            if (test_res) {
+                sb.append("== \"");
+            } else {
+                sb.append("!= \"");
+            }
+            sb.append(Cmd_Item_Valid_Result_Func.To_String(test_item.Res));
+            sb.append("\"");
+            if (test_res) {
+                sb.append(" - Ok");
+            } else {
+                sb.append("- Failed");
+            }
+            if (!test_res) {
+                test_res_total = false; // Failed
+                test_res_total_count_failed++;
+            }
+            System.out.println(sb.toString());
+        }
+        String res_str;
+        if (test_res_total) {
+            res_str = Test_Name + " - Ok (" + Integer.toString(test_items.length) + ")\n";
+        } else {
+            res_str = Test_Name + " - Failed (" + Integer.toString(test_res_total_count_failed) + " of " + Integer.toString(test_items.length) + ")\n";
+        }
+        System.out.println(res_str);
+        if (!test_res_total) {
+            fail(res_str);
+        }
+    }
+
+}
