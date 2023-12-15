500eb996b6940e0cd4cf3c5163f043f50e3c3e11
mike <guliaevm@yahoo.com>
2023-12-15 11:37:12 +0400

    Cli_Core_JavaFX: Cmd_Item_Date/Test - added (not used)

---
+++ Cli_Core_JavaFX/src/cli_core_javafx_test/Cmd_Item_Date.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cli_core_javafx_test;
+
+/**
+ *
+ * @author mike
+ */
+public class Cmd_Item_Date extends Cmd_Item_Base {
+
+    @Override
+    protected boolean Is_Char_Valid(char c, int pos, int len) {
+        switch (pos) {
+            case 0:
+            case 11:
+                return (c == '"');
+            case 5:
+            case 8:
+                return (c == '-');
+        }
+        return (c >= '0' && c <= '9');
+    }
+
+    public Cmd_Item_Date(String text, String help) {
+        super(text, help);
+        Type = "Date";
+    }
+
+    @Override
+    public String Debug_Value_Get() {
+        return "\"2018-01-02\"";
+    }
+
+    @Override
+    public Cmd_Item_Valid_Result Parse(String s) {
+        Value_Str = s;
+
+        if (s.length() == 0) {
+            return Cmd_Item_Valid_Result.CMD_ITEM_EMPTY;
+        }
+
+        if (s.length() == 1 && s.charAt(0) == '"') {
+            return Cmd_Item_Valid_Result.CMD_ITEM_INCOMPLETE_STR;
+        }
+        if (s.length() >= 1 && s.charAt(0) != '"') {
+            return Cmd_Item_Valid_Result.CMD_ITEM_ERROR;
+        }
+
+        for (int pos = 0; pos < s.length(); pos++) {
+            if (!Is_Char_Valid(s.charAt(pos), pos, s.length())) {
+                return Cmd_Item_Valid_Result.CMD_ITEM_ERROR;
+            }
+        }
+
+        // Date
+        if (s.length() >= 7) {
+            int m = s.charAt(6) - '0';
+            if (m < 0 || m > 1) {
+                return Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE;
+            }
+            if (s.length() >= 8) {
+                String s_mm = s.substring(6, 8);
+                int mm = Integer.parseInt(s_mm);
+                if (mm == 0 || mm > 12) {
+                    return Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE;
+                }
+            }
+        }
+
+        if (s.length() >= 10) {
+            int d = s.charAt(9) - '0';
+            if (d < 0 || d > 3) {
+                return Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE;
+            }
+            if (s.length() >= 11) {
+                String s_dd = s.substring(9, 11);
+                int dd = Integer.parseInt(s_dd);
+                if (dd == 0 || dd > 31) {
+                    return Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE;
+                }
+            }
+        }
+
+        if (s.length() == 2) {
+            if (s.charAt(0) == '"' && s.charAt(1) == '"') {
+                return Cmd_Item_Valid_Result.CMD_ITEM_ERROR; //Cmd_Item_Valid_Result.CMD_ITEM_OK;
+            }
+            return Cmd_Item_Valid_Result.CMD_ITEM_INCOMPLETE_STR;
+        }
+
+        if (s.charAt(s.length() - 1) != '"') {
+            return Cmd_Item_Valid_Result.CMD_ITEM_INCOMPLETE_STR;
+        }
+
+        return Cmd_Item_Valid_Result.CMD_ITEM_OK;
+    }
+
+}
---
+++ Cli_Core_JavaFX/test/cli_core_javafx_test/Cmd_Item_DateTest.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cli_core_javafx_test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mike
+ */
+public class Cmd_Item_DateTest {
+
+    String Test_Name = this.getClass().getSimpleName();
+
+    Cmd_Item_Date v = new Cmd_Item_Date("", "");
+
+    class Cmd_Item_Test_Pair {
+
+        String Text;
+        Cmd_Item_Valid_Result Res;
+
+        Cmd_Item_Test_Pair(String text, Cmd_Item_Valid_Result res) {
+            Text = text;
+            Res = res;
+        }
+    }
+
+    final Cmd_Item_Test_Pair[] test_items = {
+        new Cmd_Item_Test_Pair("", Cmd_Item_Valid_Result.CMD_ITEM_EMPTY),
+        new Cmd_Item_Test_Pair("\"<str>\"", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("\"\"", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("\"", Cmd_Item_Valid_Result.CMD_ITEM_INCOMPLETE_STR),
+        new Cmd_Item_Test_Pair("\"<str>", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("FF:FF:FF:FF:FF:FF", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("\"2018-01-18\"", Cmd_Item_Valid_Result.CMD_ITEM_OK),
+        new Cmd_Item_Test_Pair("\"2018-01-1\"", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("\"2018-01-\"", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("\"2018-01\"", Cmd_Item_Valid_Result.CMD_ITEM_ERROR),
+        new Cmd_Item_Test_Pair("\"2018-01-1", Cmd_Item_Valid_Result.CMD_ITEM_INCOMPLETE_STR),
+        new Cmd_Item_Test_Pair("\"2018-01-", Cmd_Item_Valid_Result.CMD_ITEM_INCOMPLETE_STR),
+        new Cmd_Item_Test_Pair("\"2018-01", Cmd_Item_Valid_Result.CMD_ITEM_INCOMPLETE_STR),
+        new Cmd_Item_Test_Pair("\"2018-00-01\"", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("\"2018-13-01\"", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("\"2018-01-00\"", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("\"2018-01-32\"", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("\"0000-01-01\"", Cmd_Item_Valid_Result.CMD_ITEM_OK), // DATE MIN
+        new Cmd_Item_Test_Pair("\"0000-00-01\"", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("\"0000-01-00\"", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("\"0000-13-01\"", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("\"0000-01-32\"", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("\"9999-12-31\"", Cmd_Item_Valid_Result.CMD_ITEM_OK), // DATE MAX
+        new Cmd_Item_Test_Pair("\"9999-00-31\"", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("\"9999-12-32\"", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("\"9999-13-31\"", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE),
+        new Cmd_Item_Test_Pair("\"9999-12-32\"", Cmd_Item_Valid_Result.CMD_ITEM_OUT_OF_RANGE)
+    };
+
+    @Test
+    public void Test() {
+        System.out.println(Test_Name + ":");
+        Boolean test_res_total = true; // Ok
+        int test_res_total_count_failed = 0; // Ok
+        for (int i = 0; i < test_items.length; i++) {
+            Cmd_Item_Test_Pair test_item = test_items[i];
+            Cmd_Item_Valid_Result res_cmd_valid = v.Parse(test_item.Text);
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            sb.append(Integer.toString(i));
+            sb.append("] ");
+            sb.append("Test Str: \"");
+            sb.append(test_item.Text);
+            sb.append("\" res: \"");
+            sb.append(Cmd_Item_Valid_Result_Func.To_String(res_cmd_valid));
+            sb.append("\" ");
+            Boolean test_res = (res_cmd_valid == test_item.Res);
+            if (test_res) {
+                sb.append("== \"");
+            } else {
+                sb.append("!= \"");
+            }
+            sb.append(Cmd_Item_Valid_Result_Func.To_String(test_item.Res));
+            sb.append("\"");
+            if (test_res) {
+                sb.append(" - Ok");
+            } else {
+                sb.append("- Failed");
+            }
+            if (!test_res) {
+                test_res_total = false; // Failed
+                test_res_total_count_failed++;
+            }
+            System.out.println(sb.toString());
+        }
+        String res_str;
+        if (test_res_total) {
+            res_str = Test_Name + " - Ok (" + Integer.toString(test_items.length) + ")\n";
+        } else {
+            res_str = Test_Name + " - Failed (" + Integer.toString(test_res_total_count_failed) + " of " + Integer.toString(test_items.length) + ")\n";
+        }
+        System.out.println(res_str);
+        if (!test_res_total) {
+            fail(res_str);
+        }
+    }
+
+}
