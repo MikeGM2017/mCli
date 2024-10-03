eb39b3c1260bb51dc7f28d0111f17f82096b9ed4
mike <guliaevm@yahoo.com>
2024-10-03 10:21:02 +0400

    Cli_Core_CS: Cli_Module_Vars V0.06: Int_To_Str(...) - added, var_delete(...) - changed

--- Cli_Core_CS/Cli_Core_CS/Cli_Module_Vars.cs
+++ Cli_Core_CS/Cli_Core_CS/Cli_Module_Vars.cs
@@ -25,6 +25,9 @@ namespace Cli_Core_CS
 
             CMD_ID_point_var_name_inc,
 
+            CMD_ID_point_var_name_add_str,
+            CMD_ID_point_var_name_stradd_str,
+
             CMD_ID_point_var_name_delete,
 
             CMD_ID_LAST
@@ -36,12 +39,12 @@ namespace Cli_Core_CS
         }
 
         public Cli_Module_Vars(Cli_Modules modules, Dictionary<string, string> values_map, Str_Filter str_filter,
-            Str_Get_Without_Commas str_without_commas,
-            Cli_Output_CS cli_output,
-            char c_single = '?', char c_multy = '*') : base("Vars")
+                Str_Get_Without_Commas str_without_commas,
+                Cli_Output_CS cli_output,
+                char c_single = '?', char c_multy = '*') : base("Vars")
         {
 
-            Version = "0.03";
+            Version = "0.06";
 
             Modules = modules;
             Values_Map = values_map;
@@ -96,6 +99,29 @@ namespace Cli_Core_CS
             }
 
             {
+                // .<var_name> add <str>
+                Cli_Cmd cmd = new Cli_Cmd((int)Local_Cmd_ID.CMD_ID_point_var_name_add_str);
+                cmd.Text_Set(".<var_name> add <str>");
+                cmd.Help_Set("<var_name> add int/var/str");
+                cmd.Is_Global_Set(true);
+                cmd.Item_Add(new Cmd_Item_Point_Var_Name(".<var_name>", "var name", C_Single, C_Multy));
+                cmd.Item_Add(new Cmd_Item_Word("add", "add"));
+                cmd.Item_Add(new Cmd_Item_Str("<str>", "value to add")); // <str> . .v/int/str
+                Cmd_Add(cmd);
+            }
+            {
+                // .<var_name> stradd <str>
+                Cli_Cmd cmd = new Cli_Cmd((int)Local_Cmd_ID.CMD_ID_point_var_name_stradd_str);
+                cmd.Text_Set(".<var_name> stradd <str>");
+                cmd.Help_Set("<var_name> add var/str as str");
+                cmd.Is_Global_Set(true);
+                cmd.Item_Add(new Cmd_Item_Point_Var_Name(".<var_name>", "var name", C_Single, C_Multy));
+                cmd.Item_Add(new Cmd_Item_Word("stradd", "add as str")); // <str> . .v/str
+                cmd.Item_Add(new Cmd_Item_Str("<str>", "value to add as str"));
+                Cmd_Add(cmd);
+            }
+
+            {
                 // delete
                 Cli_Cmd cmd = new Cli_Cmd((int)Local_Cmd_ID.CMD_ID_point_var_name_delete);
                 cmd.Text_Set(".<var_name> delete");
@@ -106,6 +132,68 @@ namespace Cli_Core_CS
                 Cmd_Add(cmd);
             }
 
+        }
+
+        int Str_To_Int(string s)
+        {
+            int v = 0;
+            Int32.TryParse(s, out v);
+            return v;
+        }
+
+        string Int_To_Str(int v)
+        {
+            return v.ToString();
+        }
+
+        bool Char_Is_Digit(char c)
+        {
+            if (c >= '0' && c <= '9')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool Char_Is_Digit_Or_Plus_Or_Minus(char c)
+        {
+            if (c >= '0' && c <= '9')
+            {
+                return true;
+            }
+            if (c == '+')
+            {
+                return true;
+            }
+            if (c == '-')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool Str_Is_Int(string s)
+        {
+            if (s.Length == 0)
+            {
+                return false; // Case: empty string is not digit
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (i == 0)
+                {
+                    if (!Char_Is_Digit_Or_Plus_Or_Minus(c))
+                    {
+                        return false;
+                    }
+                }
+                else if (!Char_Is_Digit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         bool var_get(string point_var_name_str)
@@ -178,14 +266,12 @@ namespace Cli_Core_CS
             bool found = false;
             foreach (var item in Values_Map)
             {
-                if (item.Key == var_name)
+                string item_var_name = item.Key;
+                if (var_name == item_var_name)
                 {
-                    int item_var_value_int_new = 0;
-                    Int32.TryParse(item.Value, out item_var_value_int_new);
-                    item_var_value_int_new++;
-                    Values_Map[var_name] = item_var_value_int_new.ToString();
+                    int item_var_value_int_new = Str_To_Int(item.Value) + 1;
+                    Values_Map[item_var_name] = Int_To_Str(item_var_value_int_new);
                     found = true;
-                    break;
                 }
             }
             if (!found)
@@ -196,32 +282,104 @@ namespace Cli_Core_CS
             return true;
         }
 
-        bool var_delete(string point_var_name_str)
+        bool var_add(string point_var_name_str, string add_str, bool is_try_add_int)
         {
-            Dictionary<string, string> new_dictionary = new Dictionary<string, string>();
             string var_name = point_var_name_str.Substring(1);
-            bool found_to_delete = false;
+            bool found = false;
             foreach (var item in Values_Map)
             {
                 string item_var_name = item.Key;
-                if (!Str_Filter.Is_Match(var_name, item_var_name))
+                if (var_name == item_var_name)
                 {
-                    new_dictionary.Add(item.Key, item.Value);
-                }
-                else
-                {
-                    found_to_delete = true;
+                    string item_var_value_old = item.Value;
+                    if (is_try_add_int && Str_Is_Int(item_var_value_old) && Str_Is_Int(Str_Without_Commas.Get(add_str)))
+                    { // Case 1: VAR_INT add VALUE_INT
+                        int item_var_value_int_old = Str_To_Int(item_var_value_old);
+                        int add_value_int = Str_To_Int(Str_Without_Commas.Get(add_str));
+                        int item_var_value_int_new = item_var_value_int_old + add_value_int;
+                        Values_Map[var_name] = Int_To_Str(item_var_value_int_new);
+                    }
+                    else if (add_str.Length > 0 && add_str[0] == '.')
+                    { // Case 2: VAR add VAR
+                        KeyValuePair<string, string> item_right = Values_Map_Find_By_Var_Name(add_str);
+                        if (item_right.Key != null)
+                        {
+                            string add_var_value = item_right.Value;
+                            if (is_try_add_int && Str_Is_Int(item_var_value_old) && Str_Is_Int(add_var_value))
+                            { // Case 2.1: VAR_INT add VAR_INT
+                                int item_var_value_int_old = Str_To_Int(item_var_value_old);
+                                int add_value_int = Str_To_Int(add_var_value);
+                                int item_var_value_int_new = item_var_value_int_old + add_value_int;
+                                Values_Map[var_name] = Int_To_Str(item_var_value_int_new);
+                            }
+                            else
+                            { // Case 2.2: VAR add VAR as str
+                                string item_var_value_new = item_var_value_old + add_var_value;
+                                Values_Map[var_name] = item_var_value_new;
+                            }
+                        }
+                        else
+                        {
+                            Cli_Output.Output_Str(add_str + " - Not Found");
+                            Cli_Output.Output_NewLine();
+                        }
+                    }
+                    else
+                    { // Case 3: VAR add STR
+                        string item_var_value_new = item_var_value_old + Str_Without_Commas.Get(add_str);
+                        Values_Map[var_name] = item_var_value_new;
+                    }
+                    found = true;
+                    break;
                 }
             }
-            if (found_to_delete)
+            if (!found)
             {
+                Cli_Output.Output_Str(var_name + " - Not Found");
+                Cli_Output.Output_NewLine();
+                if (add_str.Length > 0 && add_str[0] == '.')
+                {
+                    KeyValuePair<string, string> item_right = Values_Map_Find_By_Var_Name(add_str.Substring(1));
+                    if (item_right.Key == null)
+                    {
+                        Cli_Output.Output_Str(add_str + " - Not Found");
+                        Cli_Output.Output_NewLine();
+                    }
+                }
+            }
+            return true;
+        }
+
+        bool var_delete(string point_var_name_str)
+        {
+            string var_name = point_var_name_str.Substring(1);
+            bool found = false;
+            foreach (var item in Values_Map)
+            {
+                string item_var_name = item.Key;
+                if (Str_Filter.Is_Match(var_name, item_var_name))
+                {
+                    found = true;
+                }
+            }
+            if (found)
+            {
+                Dictionary<string, string> map_new = new Dictionary<string, string>();
+                foreach (var item in Values_Map)
+                {
+                    string item_var_name = item.Key;
+                    if (!Str_Filter.Is_Match(var_name, item_var_name))
+                    {
+                        map_new.Add(item.Key, item.Value);
+                    }
+                }
                 Values_Map.Clear();
-                foreach (var item in new_dictionary)
+                foreach (var item in map_new)
                 {
                     Values_Map.Add(item.Key, item.Value);
                 }
             }
-            if (!found_to_delete)
+            if (!found)
             {
                 Cli_Output.Output_Str(var_name + " - Not Found");
                 Cli_Output.Output_NewLine();
@@ -262,6 +420,23 @@ namespace Cli_Core_CS
                     {
                         string point_var_name_str = cmd.Items[0].Value_Str;
                         return var_inc(point_var_name_str);
+                    }
+
+                case Local_Cmd_ID.CMD_ID_point_var_name_add_str:
+                    if (is_debug) return true;
+                    {
+                        string point_var_name_str = cmd.Items[0].Value_Str;
+                        string add_str = cmd.Items[2].Value_Str;
+                        bool is_try_add_int;
+                        return var_add(point_var_name_str, add_str, is_try_add_int = true);
+                    }
+                case Local_Cmd_ID.CMD_ID_point_var_name_stradd_str:
+                    if (is_debug) return true;
+                    {
+                        string point_var_name_str = cmd.Items[0].Value_Str;
+                        string add_str = cmd.Items[2].Value_Str;
+                        bool is_try_add_int;
+                        return var_add(point_var_name_str, add_str, is_try_add_int = false);
                     }
 
                 case Local_Cmd_ID.CMD_ID_point_var_name_delete:
