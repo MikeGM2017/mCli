995f4793cd6290e7f27a9b1d4096526ea1c1d2e8
mike <guliaevm@yahoo.com>
2024-10-17 12:50:38 +0400

    Cli_Input_Test_Key_Codes_C_libssh: All Key Codes: Enter/TAB/.../UP/DOWN/LEFT/RIGHT...

--- Cli_Input_Test_Key_Codes_C_libssh/Cli_Input_Test_Key_Codes_C_libssh_main.c
+++ Cli_Input_Test_Key_Codes_C_libssh/Cli_Input_Test_Key_Codes_C_libssh_main.c
@@ -253,18 +253,92 @@ static int data_function(ssh_session session, ssh_channel channel, void *data,
         return 0;
     }
 
-    char *data_ptr = (char *) data;
+    char *d = (char *) data;
     char wr_buf[1024];
     int wr_buf_len = 0;
     int i;
     char c;
+    int processed = 0;
+
     if (len > 0) {
         wr_buf_len += sprintf(wr_buf + wr_buf_len, "\r\n");
     }
-    for (i = 0; i < len; i++) {
-        c = data_ptr[i];
-        wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X", c & 0xFF);
+    if (len == 1 && d[0] == 0x1B) { // Esc
+        wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X - Esc", d[0]);
+        processed = 1;
+    } else if (len == 3) {
+        if (d[0] == 0x1B && d[1] == 0x5B && d[2] == 0x5A) {
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X 0x%02X 0x%02X - SHIFT+TAB", d[0], d[1], d[2]);
+            processed = 1;
+        } else if (d[0] == 0x1B && d[1] == 0x5B && d[2] == 0x41) {
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X 0x%02X 0x%02X - UP", d[0], d[1], d[2]);
+            processed = 1;
+        } else if (d[0] == 0x1B && d[1] == 0x5B && d[2] == 0x42) {
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X 0x%02X 0x%02X - DOWN", d[0], d[1], d[2]);
+            processed = 1;
+        } else if (d[0] == 0x1B && d[1] == 0x5B && d[2] == 0x43) {
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X 0x%02X 0x%02X - RIGHT", d[0], d[1], d[2]);
+            processed = 1;
+        } else if (d[0] == 0x1B && d[1] == 0x5B && d[2] == 0x44) {
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X 0x%02X 0x%02X - LEFT", d[0], d[1], d[2]);
+            processed = 1;
+        } else if (d[0] == 0x1B && d[1] == 0x5B && d[2] == 0x48) {
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X 0x%02X 0x%02X - HOME", d[0], d[1], d[2]);
+            processed = 1;
+        } else if (d[0] == 0x1B && d[1] == 0x5B && d[2] == 0x46) {
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X 0x%02X 0x%02X - END", d[0], d[1], d[2]);
+            processed = 1;
+        }
+    } else if (len == 4) {
+        if (d[0] == 0x1B && d[1] == 0x5B && d[2] == 0x33 && d[3] == 0x7E) {
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X 0x%02X 0x%02X 0x%02X - DELETE", d[0], d[1], d[2], d[3]);
+            processed = 1;
+        } else if (d[0] == 0x1B && d[1] == 0x5B && d[2] == 0x32 && d[3] == 0x7E) {
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X 0x%02X 0x%02X 0x%02X - INSERT", d[0], d[1], d[2], d[3]);
+            processed = 1;
+        } else if (d[0] == 0x1B && d[1] == 0x5B && d[2] == 0x34 && d[3] == 0x7E) {
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X 0x%02X 0x%02X 0x%02X - END", d[0], d[1], d[2], d[3]);
+            processed = 1;
+        } else if (d[0] == 0x1B && d[1] == 0x5B && d[2] == 0x31 && d[3] == 0x7E) {
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X 0x%02X 0x%02X 0x%02X - HOME", d[0], d[1], d[2], d[3]);
+            processed = 1;
+        } else if (d[0] == 0x1B && d[1] == 0x5B && d[2] == 0x35 && d[3] == 0x7E) {
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X 0x%02X 0x%02X 0x%02X - PAGEUP", d[0], d[1], d[2], d[3]);
+            processed = 1;
+        } else if (d[0] == 0x1B && d[1] == 0x5B && d[2] == 0x36 && d[3] == 0x7E) {
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X 0x%02X 0x%02X 0x%02X - PAGEDOWN", d[0], d[1], d[2], d[3]);
+            processed = 1;
+        }
     }
+    if (!processed) {
+        for (i = 0; i < len; i++) {
+            c = d[i];
+            wr_buf_len += sprintf(wr_buf + wr_buf_len, " 0x%02X", c & 0xFF);
+            if (c == 0x09) {
+                wr_buf_len += sprintf(wr_buf + wr_buf_len, " - TAB");
+            } else if (c == 0x0D) {
+                wr_buf_len += sprintf(wr_buf + wr_buf_len, " - Enter");
+            } else if (c == 0x03) {
+                wr_buf_len += sprintf(wr_buf + wr_buf_len, " - Ctrl+C");
+            } else if (c == 0x1A) {
+                wr_buf_len += sprintf(wr_buf + wr_buf_len, " - Ctrl+Z");
+            } else if (c == 0x1C) {
+                wr_buf_len += sprintf(wr_buf + wr_buf_len, " - Ctrl+Backslash");
+            } else if (c == 0x7F) {
+                wr_buf_len += sprintf(wr_buf + wr_buf_len, " - Back");
+            } else if (c == 0x08) {
+                wr_buf_len += sprintf(wr_buf + wr_buf_len, " - Shift+Back");
+            } else if (c == 0x1B) {
+                wr_buf_len += sprintf(wr_buf + wr_buf_len, " - Esc");
+            } else {
+                if (isprint(c))
+                    wr_buf_len += sprintf(wr_buf + wr_buf_len, " - \'%c\'", c);
+                else
+                    wr_buf_len += sprintf(wr_buf + wr_buf_len, " - .");
+            }
+        }
+    }
+
     if (wr_buf_len > 0) {
         ssh_channel_write(channel, wr_buf, wr_buf_len);
     }
