c6f77ce53c1d0bb32ab21649ffea7ce3450ba751
mike <guliaevm@yahoo.com>
2025-02-14 11:23:39 +0400

    Cli_Input_Test_Key_Codes_CPP_win32api: Test Key_Codes_CPP_win32api - added

---
+++ Cli_Input_Test_Key_Codes_CPP_win32api/nbproject/Makefile-Debug.mk
@@ -0,0 +1,84 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc.exe
+CCC=g++.exe
+CXX=g++.exe
+FC=gfortran
+AS=as.exe
+
+# Macros
+CND_PLATFORM=GNU_DevCpp6_3-Linux
+CND_DLIB_EXT=so
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/Cli_Input_Test_Key_Codes_CPP_win32api_main.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=-fpermissive
+CXXFLAGS=-fpermissive
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=-lgdi32
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cli_input_test_key_codes_cpp_win32api
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cli_input_test_key_codes_cpp_win32api: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cli_input_test_key_codes_cpp_win32api ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/Cli_Input_Test_Key_Codes_CPP_win32api_main.o: Cli_Input_Test_Key_Codes_CPP_win32api_main.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Cli_Input_Test_Key_Codes_CPP_win32api_main.o Cli_Input_Test_Key_Codes_CPP_win32api_main.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cli_input_test_key_codes_cpp_win32api
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
